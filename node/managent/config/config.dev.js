31e74352f793a262d397d8da95642e920e510672
Zoexyf <1270355646@qq.com>
2019-12-21 17:56:50 +0800

    no

--- node/managent/config/config.dev.js
+++ node/managent/config/config.dev.js
@@ -1,8 +1,17 @@
+const path=require('path')
+
 module.exports={
     //database
     DB_HOST:'localhost',
-    DB_PORT:3306,
-    DB_USER:global,
+    DB_PORT:3308,
+    DB_USER:'root',
     DB_PASS:'',
     DB_NAME:'20191217',
+
+    //http
+
+    HTTP_PORT:8080,
+    HTTP_ROOT:path.resolve(__dirname,'../static/'),
+    HTTP_UPLOAD:path.resolve(__dirname,'../static/upload'),
+
 }
---
+++ node/managent/libs/router.js
@@ -0,0 +1,27 @@
+//路由表——JSON
+let router={};
+
+function addRouter(method,url,fn){
+    method=method.toLowerCase();
+    url=url.toLowerCase();
+    router[method]=router[method]||{};
+    router[method][url]=fn;
+
+}
+
+function findRouter(method,url){
+    method=method.toLowerCase();
+    url=url.toLowerCase();
+
+    if(!router[method]||!router[methood][url]){
+        return null;
+    }else{
+        return router[method][url]
+    }
+
+}
+
+module.exports={
+    addRouter, findRouter
+  };
+  
