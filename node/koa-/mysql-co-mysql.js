3cf10fb718c1b486ad5f12181cfc13700adac2eb
Zoexyf <1270355646@qq.com>
2020-02-10 19:08:03 +0800

    koa

--- node/koa-/mysql-co-mysql.js
+++ node/koa-/mysql-co-mysql.js
@@ -7,10 +7,19 @@ let conn=mysql.createPool({
     host:'localhost',
     user:'root',
     password:'',
-    database:''
+    database:'20191217'
 })
 
+let db=co(conn);
+
 let server=new Koa()
-server.listen(8080);
+server.listen(8000);
+
+server.context.db=db;
+//全局声明以后，可以用ctx.db直接使用，比较方便
+server.use(async ctx=>{
+   let data=await ctx.db.query('SELECT * FROM item_table')
+   ctx.body=data;
+})
 
 
